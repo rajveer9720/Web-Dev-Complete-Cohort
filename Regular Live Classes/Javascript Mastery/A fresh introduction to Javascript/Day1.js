7a34f1c7f8977996d9067ff8245ff5ad702610ce
rajveer9720 <rajveersingh05021999@gmail.com>
2025-08-06 23:53:48 +0530

    Learn Day 1 about Functions  and added readme

---
+++ Regular Live Classes/Javascript Mastery/A fresh introduction to Javascript/Day1.js
@@ -0,0 +1,16 @@
+function aryanTest() {
+    console.log("Brush le aaya lo karlo jaldi brush ");
+}
+
+function bringBrush(itne) {
+    console.log(`Jaldi Jao or ${itne} Brush Leke Aao`)
+}
+function add(add1, add2) {
+    return add1 + add2;
+}
+
+
+bringBrush(4);
+aryanTest();
+add(2, 5);
+console.log(add(2, 9))
---
+++ Regular Live Classes/Javascript Mastery/readme.md
@@ -0,0 +1,6 @@
+# What is functions?
+Functions like a factory of piece of code which factory works like a functions
+
+![alt text](image.png)
+
+![alt text](image-1.png)
